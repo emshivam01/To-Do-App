c6b9733f163ed4144c82cca7a6bdee87f7ed00fb
Shivam Sharma <75516122+emshivam01@users.noreply.github.com>
2023-01-26 09:55:15 +0530

    Delete routes directory

--- routes/TodoRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { home, createTodo } = require("../controllers/TodoController");
-
-const router = express.Router();
-
-router.get("/", home);
-router.post("/createTodo", createTodo);
-
-module.exports = router;
